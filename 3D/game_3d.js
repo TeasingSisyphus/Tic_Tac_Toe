f5cd59737f90178e1e3f233484ea0bf70ec72ee4
itsalaidbacklife <itsalaidbacklife@gmail.com>
2014-04-08 13:29:43 -0400

    working on render method

--- 3D/game_3d.js
+++ 3D/game_3d.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+  alert('loaded!');
   //The template game object
   var Game = function() {
     this.turn = 0;
@@ -241,6 +242,42 @@ window.onload = function() {
       game.check_board_column();
       game.check_board_row_column();
     }
+
+    //Renders game board base on array
+    /*    this.render = function() {
+      //alert('RENDERIN');
+      var self = this;
+      $('.place').each(function() {
+          //alert('looping');
+          //Fetches index from place clicked
+          //Fetches board index based on first letter of class
+          if ($(this).prop('class').charAt(0) == 'w') {
+            alert("it's w!");
+            var b_index = 0;
+          } else if ($(this).prop('class').charAt(0) == 'x') {
+            var b_index = 1;
+          } else if ($(this).prop('class').charAt(0) == 'y') {
+            var b_index = 2;
+          } else if ($this).prop('class').charAt(0) == 'z') {
+          var b_index = 3;
+        }
+        alert('mid-loop');
+        //raw_index is taken from class of event target, then converted to a row and column indexes
+        var raw_index = $(this).prop('class'); raw_index = raw_index.replace(/[^\d]/g, '');
+        var c_index = (parseInt(raw_index) + 3) % 4;
+        if (raw_index < 5) {
+          var r_index = 0;
+        } else if (raw_index < 9) {
+          var r_index = 1;
+        } else if (raw_index < 13) {
+          var r_index = 2;
+        } else {
+          var r_index = 3;
+        }
+        //Fills each place with the symbol stored corresonding spot in game.board
+        $(this).text(self.board[b_index][r_index][c_index]); alert('rendered');
+      });
+  }*/
   }
 
 
@@ -252,7 +289,6 @@ window.onload = function() {
   $('.place').click(
     function() {
       //alert('Click!');
-
       //Fetches index from place clicked
       //Fetches board index based on first letter of class
       if ($(event.target).prop('class').charAt(0) == 'w') {
@@ -294,8 +330,10 @@ window.onload = function() {
         game.board[b_index][r_index][c_index] = game.turnsymbol;
 
         //Checks for win, incriments turn and changes turnsymbol
+        //game.render();
         game.check();
         game.turn++;
+        //alert('symbol changin')
         game.turnsymbol = game.symbol[game.turn % 2];
       }
 
